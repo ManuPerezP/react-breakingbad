cbc19aa740173ab77b481328129919eb0535db82
Manuel PP <manuel.perez.pozo@gmail.com>
2021-05-09 18:28:54 -0400

    proyecto inicial

---
+++ src/components/Frase.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from "@emotion/styled";
+
+const ContenedorFrase = styled.div`
+    padding: 3rem;
+    border-radius: .5rem;
+    background-color:#fff;
+    max-width:800px;
+
+    @media(min-width: 922px){
+        margin-top: 10rem;
+    }
+
+    h1{
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        position:relative;
+        padding-left:4rem;
+        
+        &::before {
+            content: open-quote;
+            font-size: 10rem;
+            color: black;
+            position:absolute;
+         top:-4rem;
+         left:-2rem;
+        }
+        &::after {
+            content: close-quote;
+            font-size: 10rem;
+            color: black;
+            position:absolute;
+            right:-2rem;
+            bottom:-14rem;
+        }
+    }
+    p{
+            font-family:Verdana, Geneva, Tahoma, sans-serif;
+            font-size:1.4rem;
+            text-align: right;
+            color: #666;
+            margin-top: 2rem;
+            font-weight: bold;
+        }
+
+
+`;
+
+const Frase = ({frase})=>{
+
+    if(Object.keys(frase).length === 0)return null;
+
+    return (
+        <ContenedorFrase>
+        <h1>{frase.quote}</h1>
+        <p>- {frase.author}</p>
+        </ContenedorFrase>
+    )
+}
+
+export default Frase;
